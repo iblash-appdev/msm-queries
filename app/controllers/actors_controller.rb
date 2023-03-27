c1d39ae5af8f9c531c0233cc51e965c5c9ccb826
Milton Lash <iblash@live.com>
2023-03-27 18:36:59 +0000

    correction

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -5,5 +5,16 @@ class ActorsController < ApplicationController
     render({ :template => "actor_templates/index.html.erb"})
   end
 
-end
+  def actor_details
+    # params looks like {"an_id" =>"42"}
 
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id}).at(0)
+
+    @filmography  = Movie.where({ :director_id => @the_director.id })
+
+    render({ :template => "director_templates/show.html.erb" })
+  end
+
+end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,16 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/index.html.erb"})
   end
 
+  def movie_details
+    # params looks like {"an_id" =>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id}).at(0)
+
+    @filmography  = Movie.where({ :director_id => @the_director.id })
+
+    render({ :template => "director_templates/show.html.erb" })
+  end
+
 end
